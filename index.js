49bb27e7157bf5e97544851e8ca5947c2a39d75c
Rahul <rahulshinde8828@gmail.com>
2025-11-11 13:43:25 +0530

    cors

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const app = express();
 
 
 app.use(cors({
-  origin: "*", 
+  origin: "https://social-media-post-frontend.vercel.app/", 
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
@@ -19,6 +20,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use("/static", express.static("uploads"));
+app.options("*", cors());
 
 
 mongoose
