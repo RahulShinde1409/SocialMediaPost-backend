7c29090e8614b97f3bc3db040469c21895ccbb78
Rahul <rahulshinde8828@gmail.com>
2026-07-20 20:05:34 +0530

    sendEmail

--- controller/config/sendEmail.js
+++ controller/config/sendEmail.js
@@ -1,25 +1,29 @@
 import nodemailer from "nodemailer";
 
-const sendEmail = async(options)=>{
+const sendEmail = async (options) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-const transporter = nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASS
-    }
-});
+    console.log("Sending email to:", options.email);
 
-console.log("Sending email to:", options.email);
-const info = await transporter.sendMail({
-  from: process.env.EMAIL_USER,
-  to: options.email,
-  subject: options.subject,
-  text: options.message,
-});
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    });
 
-console.log(info);
-
+    console.log("SUCCESS:", info.response);
+  } catch (error) {
+    console.log("EMAIL ERROR:");
+    console.log(error);
+  }
 };
 
 export default sendEmail;
